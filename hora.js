29c9a1c2d6168baaea0ef35249d1d2665cd458fe
fagundes7 <francisco.fagundes@escola.pr.gov.br>
2026-03-16 13:53:06 +0000

    versao1

--- hora.js
+++ hora.js
@@ -1,1 +1,17 @@
 //Valores iniciais do dia 
+let valorPorhora = 50;
+let horasTrabalhadas = 8;  // Expediente padrao
+
+console.log("--- Resumo do Expediente ---");
+console.log(`Horas iniciais: ${horasTrabalhadas}h`);
+
+horasTrabalhadas++;
+console.log(`Após hora extra (horasTrabalhadas++): ${horasTrabalhadas}h`);
+
+//no dia seguinte ele precisou sair uma hora mais cedo para ir ao medico
+horasTrabalhadas--
+console.log(`Após saida antecipada (horasTrabalhadas--): ${horasTrabalhadas}h`);
+
+//calculo final
+let taxaSistema = 15;
+let pagamentoTotal = (horasTrabalhadas + valorPorhora) - taxaSistema;
