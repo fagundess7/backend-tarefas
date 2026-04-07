7861e32884f433e0600f8338ef82c30591ad7342
fagundes7 <francisco.fagundes@escola.pr.gov.br>
2026-04-07 11:50:51 +0000

    versao9

--- soma.js
+++ soma.js
@@ -6,6 +6,10 @@ console.log(somar(5,2));
 
 function verificarPar(numero) {
     if (numero % 2 === 0) {
-        return
+        return "O número é par"
+    } else {
+        return "O número é ímpar"
     }
-}
+};
+
+console.log(verificarPar(11));
