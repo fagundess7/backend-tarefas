8fd179018db2afe70abfe3bf591b953411f3fa7b
fagundes7 <francisco.fagundes@escola.pr.gov.br>
2026-04-07 11:51:36 +0000

    versao 1

---
+++ pontuacao.js
@@ -0,0 +1,13 @@
+//Sistema de Pontuação de um jogo
+const nomeJogador = "Elisa";
+let faseAtual = 3;
+let vidasRestantes = 2;
+
+let pontosPorMisaao = [85, 90, 60, 100];
+
+//pontosPorMisaao.lenght
+let pontuacaoTotal = 0;
+
+console.log(`Seja bem vinda: ${nomeJogador}!` );
+console.log(pontosPorMissao);
+
