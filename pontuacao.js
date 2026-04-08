d025f99f30df10eecfbd7c43c685e5d96834093e
fagundes7 <francisco.fagundes@escola.pr.gov.br>
2026-04-08 12:32:24 +0000

    versao 4

--- pontuacao.js
+++ pontuacao.js
@@ -8,6 +8,14 @@ let pontosPorMisaao = [85, 90, 60, 100];
 //pontosPorMisaao.lenght
 let pontuacaoTotal = 0;
 
-console.log(`Seja bem vinda: ${nomeJogador}!` );
+for (let i = 0; i < pontuacoes.lenght; i++) {
+    pontuacaoTotal = pontuacaoTotal + pontuacoes[i]
+}
+//Calculando a média
+let media = pontuacaoTotal / pontuacoes.lenght;
+
+let passouDeFase = (media >= 75 && vidas > 0) || (fase === 1)
+
+console.log(`Seja bem vinda: ${nomeJogador}!`);
 console.log(pontosPorMissao);
 
